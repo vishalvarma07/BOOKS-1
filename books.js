831d1193d7c198e8a3a380d61de8736050d5351e
vishalvarma07 <kovoruvishalvarma@gmail.com>
2020-04-01 19:57:29 +0530

    finished search

--- books.js
+++ books.js
@@ -1,54 +1,38 @@
-let bookContent = {
-    maths: [
-        {
-            name: 'Applied statistics and probability for engineers',
-            img: 'images/maths/appliedstatistics.jpg',
-            link: 'books/maths/applied-statistics-and-probability-for-engineers.pdf'
-        },
-        {
-            name: 'fundamentals of applied probability and randomprocess',
-            link: 'books/maths/epdf.pub_fundamentals-of-applied-probability-and-randopro.pdf',
-            img: 'images/maths/fundprob.jpg'
-        },
-        {
-            name: 'Probability and statistics for engineers and scientisst walpole',
-            link: 'books/maths/probability_and_statistics_for_engineers_and_scientisst_walpole(1).pdf',
-            img: 'images/maths/probstats.jpg'
-        },
-        {
-            name: 'Probability and Random Processes for Engineers',
-            link: 'books/maths/Probability-and-Random-Processes-for-Engineers.pdf',
-            img: 'images/maths/probrand.jpg'
-        }
-    ],
-    os: [
-        {
-            name: 'Abraham Silberschatz Operating System Concepts',
-            link: 'books/os/Abraham-Silberschatz-Operating-System-Concepts---9th2012.12.pdf',
-            img:'images/AbrahamOS.jpg'
-        }
-    ]
-};
+let images = [
+    {
+        name: 'Applied statistics and probability for engineers',
+        img: 'images/Maths/appliedstatistics.jpg',
+        link: 'books/Maths/applied-statistics-and-probability-for-engineers.pdf'
+    },
+    {
+        name: 'fundamentals of applied probability and randomprocess',
+        link: 'books/Maths/epdf.pub_fundamentals-of-applied-probability-and-randopro.pdf',
+        img: 'images/Maths/fundprob.jpg'
+    }, 
+    {
+        name: 'Probability and statistics for engineers and scientisst walpole',
+        link: 'books/Maths/probability_and_statistics_for_engineers_and_scientisst_walpole(1).pdf',
+        img: 'images/Maths/probstats.jpg'
+    },
+    {
+        name: 'Probability and Random Processes for Engineers',
+        link: 'books/Maths/Probability-and-Random-Processes-for-Engineers.pdf',
+        img: 'images/Maths/probrand.jpg'
+    },
+    {
+        name: 'Abraham Silberschatz Operating System Concepts',
+        link: 'books/os/Abraham-Silberschatz-Operating-System-Concepts---9th2012.12.pdf',
+        img: 'images/os/AbrahamOS.jpg'
+    },
+];
 const tbnames = document.getElementById('maths');
-mathsDiv = document.getElementById('mathBooks');
-osDiv = document.getElementById('osBooks');
+ptag = document.getElementById('mathBooks');
+div = document.getElementById('searchpanel')
 function find(){
     let x = document.getElementById('searchbar').value.toUpperCase();
-    if(x.length === 0) {
-        tbnames.innerHTML = 'MATHS';
-    } else {
-        tbnames.innerHTML = 'Results:';
-    }
-    mathsDiv.innerHTML = '';
-    osDiv.innerHTML = '';
+    div.innerHTML = '';
     let newImages = [];
-    let allElements = [];
-    for(let v in bookContent) {
-        for(let k in bookContent[v]) {
-            allElements.push(bookContent[v][k]);
-        }
-    }
-    for(let v of allElements){
+    for(let v of images){
         if(v.name.toUpperCase().indexOf(x) >  -1){
             newImages.push(v);
         }
@@ -60,25 +44,13 @@ function find(){
         img.setAttribute('class', 'img-container');
         img.setAttribute('src', v.img);
         book.appendChild(img);
-        switch(getBookType(v)) {
-            case 'maths': {
-                mathsDiv.appendChild(book);
-                break;
-            }
-            case 'os': {
-                osDiv.appendChild(book);
-                break;
-            }
-        }
+        div.appendChild(book);
     }
-}
-function getBookType(item) {
-    for(let v of Object.keys(bookContent)) {
-        for(let k of Object.keys(bookContent[v])) {
-            if(bookContent[v][k] === item) {
-                return v;
-            }
-        }
+    if(x.length === 0) {
+        tbnames.innerHTML = "MATHS";
+        div.innerHTML = '';
+    } else {
+        tbnames.innerHTML = 'Results:';
     }
 }
 (() => {
