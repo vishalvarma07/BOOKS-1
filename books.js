6c31855e193de0434fa9e24e002b5dac5677fb38
vishalvarma07 <55092724+vishalvarma07@users.noreply.github.com>
2020-04-01 12:17:49 +0530

    Update books.js

--- books.js
+++ books.js
@@ -1,33 +1,8 @@
-//document.write("hello this is Vishal Varma");
-
-function verify(){
-    var bool = false;
-    while(bool == false){
-        var x = prompt("Enter username");
-        var y = prompt("Enter the password");
-        if(x == "cb.en.u4cse18334"){
-            if(y == "cse90287"){
-                bool = true;
-            }
-            else{
-                alert("Either ur password or username is incorrect");
-            }
-        }
-        else if(x == "cb.en.u4cse18352"){
-            if(y == "cse65699"){
-                bool = true;
-            }
-            else{
-                alert("Either ur password or username is incorrect");
-            }
-        }
-        else{
-            alert("Either ur password or username is incorrect");
-        }
-    }
-    return bool;
+const tbnames = document.getElementById('maths');
+function find(){
+    var x = document.getElementById('searchbar').value;
+    document.getElementById('maths').innerHTML = "value"+ x;
 }
-//var verification = verify();
-if(verification == true){
-
-}
+(() => {
+    document.getElementById('searchbar').addEventListener('change',find);
+})();
